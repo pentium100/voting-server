93234e89fcd8bdf7caf9477afe0dd393b9f7cb1e
Liwei Chen <liwei@itg.com.cn>
2015-11-17 12:02:06 +0800

    day 1

---
+++ test/reducer_spec.js
@@ -0,0 +1,26 @@
+import { Map, fromJS } from 'immutable';
+
+import { expect } from 'chai';
+
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+  it('handles SET_ENTRIES', () => {
+
+    const initialState = Map();
+    const action = {
+      type: 'SET_ENTRIES',
+      entries: ['Trainspotting']
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['Trainspotting']
+    }));
+
+
+  });
+
+})
